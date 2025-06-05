f77d88dd46e775d82cc5489ebcaf63fa30cf3b85
Pavlo Lekhitskyi <pashokman1@gmail.com>
2025-06-05 10:27:07 +0300

    added/changed some comments

--- conftest.py
+++ conftest.py
@@ -12,7 +12,6 @@ from utils.logs.logger import Logger
 @pytest.fixture()
 def login(page: Page, request):
     # Open and return Login page and prepare it to logging process
-
     browser_name = request.config.getoption("--browser")
     test_file = request.node.fspath
     test_name = request.node.name
@@ -28,8 +27,7 @@ def login(page: Page, request):
 
 @pytest.fixture(scope="function", autouse=True)
 def before_and_after_each_test(request):
-    # Add START and END marks before each test to separate them.
-
+    # Add START and END marks in logs before each test to separate them.
     test_file = request.node.fspath
     test_name = request.node.name
     browser_name = request.config.getoption("--browser")
@@ -48,17 +46,16 @@ def before_and_after_each_test(request):
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_sessionstart(session):
-    # Before each test run clear "allure-results" and "test-results" folders
-    
+    # Before each pytest run, clear "allure-results" and "test-results" folders
     allure_dir = os.path.join(os.getcwd(), "allure-results")
     if os.path.exists(allure_dir):
         shutil.rmtree(allure_dir)
         os.makedirs(allure_dir)
 
-    img_vid_dir = os.path.join(os.getcwd(), "test-results")
-    if os.path.exists(img_vid_dir):
-        shutil.rmtree(img_vid_dir)
-        os.makedirs(img_vid_dir)
+    img_vid_trace_dir = os.path.join(os.getcwd(), "test-results")
+    if os.path.exists(img_vid_trace_dir):
+        shutil.rmtree(img_vid_trace_dir)
+        os.makedirs(img_vid_trace_dir)
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
--- utils/files_worker/get_credentials.py
+++ utils/files_worker/get_credentials.py
@@ -4,7 +4,6 @@ import csv
 def get_credentials(file_path):
     with open(file_path, "r", newline="") as f:
         reader = csv.DictReader(f)
-        headers = reader.fieldnames
         rows = list(reader)
 
         return rows
