4a229c91c3ab31b40adeff7feea859610b5f029b
Pavlo Lekhitskyi <pashokman1@gmail.com>
2025-06-11 11:07:31 +0300

    added should_xfail column into a login_credential.csv file and change test to accept this parameter

--- tests/test_try.py
+++ tests/test_try.py
@@ -5,12 +5,18 @@ from utils.files_worker.get_credentials import get_credentials
 CREDENTIALS = get_credentials("./data/login_credentials.csv")
 
 
+PARAMS = [
+    pytest.param(
+        cred,
+        id=f"{cred['username'] or 'empty'}-{cred['password'] or 'empty'}",
+        marks=pytest.mark.xfail(reason="Expected fail") if cred.get("should_xfail") else (),
+    )
+    for cred in CREDENTIALS
+]
+
+
 @pytest.mark.smoke
-@pytest.mark.parametrize(
-    "credentials",
-    CREDENTIALS,
-    ids=[f"{cred['username'] or 'empty'}-{cred['password'] or 'empty'}" for cred in CREDENTIALS],
-)
+@pytest.mark.parametrize("credentials", PARAMS)
 def test_item_names_text(login, credentials) -> None:
     login.login(credentials["username"], credentials["password"])
     login.is_backpack_item_name_contain_text("Sauce Labs Backpack")
--- utils/files_worker/get_credentials.py
+++ utils/files_worker/get_credentials.py
@@ -2,8 +2,10 @@ import csv
 
 
 def get_credentials(file_path):
+    creds = []
     with open(file_path, "r", newline="") as f:
         reader = csv.DictReader(f)
-        rows = list(reader)
-
-        return rows
+        for row in reader:
+            row["should_xfail"] = row["should_xfail"].strip().lower() == "true"
+            creds.append(row)
+    return creds
